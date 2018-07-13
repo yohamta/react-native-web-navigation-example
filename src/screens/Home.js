a45cd9c256d8f52afbce41485e8bc06b937b446e
hayo0914 <hayo0914@gmail.com>
2018-07-13 18:55:47 +0900

    Change background color for visibility

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native-web';
 
 export default () => (
-  <View style={{ flex: 1, justifyContent: 'center' }}>
+  <View style={{ flex: 1, justifyContent: 'center', backgroundColor: 'skyblue' }}>
     <Text style={{ textAlign: 'center' }}>This is Home Screen!</Text>
   </View>
 );
--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native-web';
 
 export default () => (
-  <View style={{ flex: 1, justifyContent: 'center' }}>
+  <View style={{ flex: 1, justifyContent: 'center', backgroundColor: 'green' }}>
     <Text style={{ textAlign: 'center' }}>This is Settings Screen!</Text>
   </View>
 );
