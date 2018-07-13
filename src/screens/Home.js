2b49be33971c76551ac77729a113022fc7542dca
hayo0914 <hayo0914@gmail.com>
2018-07-13 18:50:32 +0900

    Fix layout

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,6 +3,6 @@ import { View, Text } from 'react-native-web';
 
 export default () => (
   <View style={{ flex: 1, justifyContent: 'center' }}>
-    <Text>This is Home Screen!</Text>
+    <Text style={{ textAlign: 'center' }}>This is Home Screen!</Text>
   </View>
 );
--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native-web';
 
 export default () => (
   <View style={{ flex: 1, justifyContent: 'center' }}>
-    <Text>This is Settings Screen!</Text>
+    <Text style={{ textAlign: 'center' }}>This is Settings Screen!</Text>
   </View>
 );
 
